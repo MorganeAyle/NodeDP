Fix initial gradient norm estimation and refactor DP training step to handle batches

src/trainer.py
@@ -1,17 +1,13 @@
-import copy
-import pdb
-
 import torch
 from torch import nn
 import torch.nn.functional as F
 import numpy as np
-import collections
-import scipy
 from src import autograd_hacks
 
 from src.models import create_model
-from src.utils import _coo_scipy2torch
 from src.constants import DP_METHODS, NON_DP_METHODS
+
+import warnings
 
 
 class Trainer:
@@ -39,6 +35,7 @@ class Trainer:
         out_channels = self.labels.shape[1]
         self.model = create_model(in_channels, out_channels, model_args)
         autograd_hacks.add_hooks(self.model)  # for the fast DP method
+        self.num_param = len([p for p in self.model.parameters()])
 
         if self.use_cuda:
             self.model.to('cuda')
@@ -55,9 +52,13 @@ class Trainer:
         if sampler_args['method'] in NON_DP_METHODS:
             self.clip_norm = training_args['clip_norm']
         elif sampler_args['method'] in DP_METHODS:
-            self.grad_norms = self.estimate_init_grad_norms(*self.minibatch.sample_one_batch(out, mode='train'))
-            self.grad_norms = training_args['C%'] * self.grad_norms
-            self.C = np.sqrt(sum(self.grad_norms ** 2))
+            clip_norm = training_args['clip_norm']
+            init_grad_norms = self.estimate_init_grad_norms(*self.minibatch.sample_one_batch(out, mode='train'))
+            if np.sqrt(sum(init_grad_norms ** 2)) < clip_norm:
+                warnings.warn("Specified clip norm is larger than initial model's clip norm. Use smaller clip_norm"
+                              "for better privacy guarantees.")
+            grad_weights = torch.nn.functional.softmax(init_grad_norms, dim=0)
+            self.per_param_clip_norm = np.sqrt(grad_weights * (clip_norm ** 2))
         else:
             raise NotImplementedError
 
@@ -79,11 +80,11 @@ class Trainer:
         count = 0
         while count < 10:
             i += 1
-            if nodes[i] in roots:
+            if nodes[0][i] in roots[0]:
                 count += 1
                 self.model.zero_grad()
-                preds = self.model(self.feats[nodes], adj)
-                loss = self._loss(preds[i].unsqueeze(0), self.labels[nodes][i].unsqueeze(0))
+                preds = self.model(self.feats[nodes[0]], adj[0])
+                loss = self._loss(preds[i].unsqueeze(0), self.labels[nodes[0]][i].unsqueeze(0))
                 grads = torch.autograd.grad(loss, list(self.model.parameters()))
                 grad_norms.append([grad.norm() for grad in grads])
         return torch.tensor(grad_norms).mean(0)
@@ -99,44 +100,45 @@ class Trainer:
         self.optimizer.step()
         return loss, self.predict(preds), self.labels[nodes]
 
-    def dp_train_step_fast(self, nodes, adj, roots=None, sigma=1.):
-        self.model.train()
-        self.optimizer.zero_grad()
-        autograd_hacks.clear_backprops(self.model)
-        for param in self.model.parameters():
-            if hasattr(param, 'grad1'):
-                del param.grad1
+    def dp_train_step_fast(self, all_nodes, all_adj, all_roots=None, sigma=1.):
+        all_grads = []
+        num_nodes = sum([len(roots) for roots in all_roots])
 
-        # compute loss only on training nodes
-        idx = [i for i, v in enumerate(nodes) if v in roots]
-        preds = self.model(self.feats[nodes], adj)
-        self._loss(preds[idx], self.labels[nodes][idx]).backward(retain_graph=True)
-        num_nodes = len(roots)
-        autograd_hacks.compute_grad1(self.model)
+        if type(all_adj) != list:
+            all_nodes = [all_nodes]
+            all_adj = [all_adj]
+            all_roots = [all_roots]
 
-        # to avoid memory problems
-        for param in self.model.parameters():
-            del param.grad
-            param.grad1 = param.grad1.cpu()
+        for nodes, adj, roots in zip(all_nodes, all_adj, all_roots):
+            self.model.train()
+            self.optimizer.zero_grad()
+            autograd_hacks.clear_backprops(self.model)
+            for param in self.model.parameters():
+                if hasattr(param, 'grad1'):
+                    del param.grad1
 
-        # compute new noisy gradients
+            # compute loss only on training nodes
+            idx = [i for i, v in enumerate(nodes) if v in roots]
+            preds = self.model(self.feats[nodes], adj)
+            self._loss(preds[idx], self.labels[nodes][idx]).backward(retain_graph=True)
+            autograd_hacks.compute_grad1(self.model)
+
+            # compute new noisy gradients
+            for i, param in enumerate(self.model.parameters()):
+                grad_norm = param.grad1.norm(dim=tuple(np.arange(len(param.grad1.shape))[1:]))[idx].flatten()
+                denom = torch.maximum(torch.tensor(1, device=param.grad1.device),
+                                      grad_norm / self.per_param_clip_norm[i])
+                for _ in range(len(param.grad1.shape) - 1):
+                    denom = denom.unsqueeze(-1)
+
+                new_grad = (param.grad1[idx] / denom).sum(0)
+                if len(all_grads) < self.num_param:
+                    all_grads.append(new_grad)
+                else:
+                    all_grads[i] += new_grad
+
         for i, param in enumerate(self.model.parameters()):
-            param_grad = param.grad1
-            grad_norm = param_grad.norm(dim=tuple(np.arange(len(param_grad.shape))[1:]))[idx]
-            denom = torch.maximum(torch.tensor([1] * num_nodes, device=grad_norm.device),
-                                  grad_norm.flatten() / self.grad_norms[i])
-            for _ in range(len(param_grad.shape) - 1):
-                denom = denom.unsqueeze(-1)
-
-            new_grad = param_grad[idx] / denom  # clip gradients
-            new_grad = new_grad.sum(0)
-            mean = torch.zeros_like(new_grad)
-            # std = torch.ones_like(new_grad) * (sigma * self.grad_norms[i]) ** 2
-            std = torch.ones_like(new_grad) * sigma
-            noise = torch.normal(mean, std)
-
-            del param.grad1
-            param.grad = ((new_grad + noise) / num_nodes).cuda()
+            noise = torch.empty(param.shape, device=param.device).normal_(mean=0.0, std=sigma)
+            param.grad = ((all_grads[i] + noise) / num_nodes)  # .cuda()
 
         self.optimizer.step()
-